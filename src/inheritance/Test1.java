d980fa31f67a945778c6ed3ef5e93f239f775d53
anilchitturi <anilkumarchitturi@gmail.com>
2014-11-20 11:18:47 +0530

    updated class code

---
+++ src/inheritance/Test1.java
@@ -0,0 +1,27 @@
+package inheritance;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Test1 extends Driver{
+
+private static WebDriver driver=null;
+
+	public static void main(String[] args) throws Exception{
+		
+		/*create a class object instance and getting the driver object
+		Driver dv=new Driver();
+		driver=dv.dranceInstance("firefox");*/
+		
+		//getting webdriver instance by extending the base class
+		//driver=dranceInstance("firefox");
+		driver.get("http://www.google.com");
+		WebElement element=driver.findElement(By.id("gbqfq"));
+		Thread.sleep(10000);
+		element.sendKeys("books");
+		element.submit();		
+		driver.quit();	
+	}
+
+}
